4ac8f0a3975c7041cb8ea81906b8df4adbd48230
snrnapa <shino.satoru@gmail.com>
2024-01-07 13:46:53 +0900

    テストデータの値を整理した

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,16 +1,20 @@
--- users初期データ
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- users初期データ-- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 insert into
     users(id, last_name, first_name, dept_id, team_id)
 select
     i,
     format('ふぁーすとねーむ%s', i),
     format('らすとねーむ%s', i),
-    round((random() * (1 - 5)) :: numeric, 0) + 5,
-    round((random() * (1 - 2)) :: numeric, 0) + 2
+    round((random() * (1 - 3)) :: numeric, 0) + 3,
+    round((random() * (1 - 10)) :: numeric, 0) + 10
 from
     generate_series(1, 1000) as i;
 
--- depts初期データ
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- depts初期データ-- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 insert into
     depts(dept_id, dept_name, leader_id)
 select
@@ -30,24 +34,26 @@ select
 from
     generate_series(1, 10) as i;
 
--- fees初期データ
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- fees初期データ-- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 insert into
     fees(user_id, seq, date, departure, arraival, price)
 select
     i,
     n,
-    CAST(
-        date '2023-11-10' + '1 day' :: INTERVAL * d AS date
-    ),
+    d,
     round((random() * (1 - 100)) :: numeric, 0) + 100,
     round((random() * (1 - 100)) :: numeric, 0) + 100,
     round((random() * (140 - 2000)) :: numeric, 0) + 2000
 from
     generate_series(1, 1000) as i,
     generate_series(1, 2) as n,
-    generate_series(1, 200) as d;
+    generate_series('2023-10-1', '2024-2-3', '1 day' :: interval) as d;
 
--- reports初期データ
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- reports初期データ-- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 insert into
     reports(
         user_id,
@@ -67,13 +73,15 @@ select
     d + cast('9 hour' AS interval) + cast(n || 'hour' AS interval),
     d + cast('10 hour' AS interval) + cast(n || 'hour' AS interval),
     round((random() * (0 - 1)) :: numeric, 0) + 1,
-    round((random() * (1000 - 1100)) :: numeric, 0) + 1100
+    round((random() * (1 - 100)) :: numeric, 0) + 100
 from
-    generate_series(1, 400) as i,
+    generate_series(1, 1000) as i,
     generate_series('2023-10-1', '2024-2-3', '1 day' :: interval) as d,
     generate_series(1, 8) as n;
 
--- projects初期データ
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- projects初期データ-- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 insert into
     projects(
         proj_id,
@@ -84,12 +92,14 @@ insert into
 select
     i,
     format('プロジェクト%s', i),
-    round((random() * (1 - 10)) :: numeric, 0) + 10,
-    round((random() * (1 - 400)) :: numeric, 0) + 400
+    round((random() * (1 - 100)) :: numeric, 0) + 100,
+    round((random() * (1 - 1000)) :: numeric, 0) + 1000
 from
-    generate_series(1000, 1100) as i;
+    generate_series(1, 100) as i;
 
--- customers初期データ
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- customers初期データ-- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 insert into
     customers(
         customer_id,
@@ -101,4 +111,4 @@ select
     format('カスタマーネーム%s', i),
     format('カスタマー情報%s', i)
 from
-    generate_series(1000, 1100) as i;
+    generate_series(1, 50) as i;
